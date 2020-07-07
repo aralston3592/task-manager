3e69883b7b2d95f08c44873b4ac14bf3decf90d0
Andrew Ralston <andrewralston35@gmail.com>
2020-07-07 05:49:16 +0000

    .gitignore created

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+if os.path.exists("env.py"):
+    import env
+
+MONGODB_URI = os.getenv("MONGO_URI")
+DBS_NAME = "task-manager"
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = 'task-manager'
-app.config["MONGO_URI"] = 'mongodb+srv://andyralston35:Tmnt3592@myfirstcluster-2yvra.mongodb.net/task-manager?retryWrites=true&w=majority'
+# app.config["MONGO_DBNAME"] = 'task-manager'
+# app.config["MONGO_URI"] = 'mongodb+srv://andyralston35:Tmnt3592@myfirstcluster-2yvra.mongodb.net/task-manager?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
@@ -14,10 +19,10 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template('tasks.html', tasks=mongo.db.tasks.find())
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
-if __name__ == '__main__':
-    app.run(host=os.getenv('IP', '0.0.0.0'),
-            port=(os.getenv('PORT', '5000')),
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP", '0.0.0.0'),
+            port=int(os.environ.get("PORT", "5000")),
             debug=True)
