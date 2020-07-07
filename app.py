03150d80fbb172fe7fe571ffea62a5b223c9904d
Andrew Ralston <andrewralston35@gmail.com>
2020-07-07 14:43:37 +0000

    fixed syntax error

--- app.py
+++ app.py
@@ -21,5 +21,5 @@ def get_tasks():
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP", '0.0.0.0'),
-            port=int(os.environ.get("PORT", "5000")),
+            port=int(os.environ.get("PORT", "8080")),
             debug=True)
